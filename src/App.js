ffac7b2504b1089fd3d952f13e95905520c6f28d
Shrey Shailesh Shah <shreyss99@gmail.com>
2024-06-21 00:24:14 -0500

    Basic Code

---
+++ src/App.js
@@ -0,0 +1,41 @@
+//import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import { HomePage } from './pages/HomePage';
+import EditorPage from './pages/EditorPage';
+
+function App() {
+  return (
+    <div className="App">
+
+      <>
+
+	  	<div>
+			<Toaster 
+				position='top-right' 
+				toastOptions={{
+					success: {
+						theme: {
+							primary: 'lightgreen'
+						},
+					},
+				}}>
+			</Toaster>
+		</div>
+
+		<BrowserRouter>
+			<Routes>
+				<Route path = "/" element={<HomePage/>}> </Route>
+				<Route path = "/editor/:roomId" element={<EditorPage/>}></Route>
+			</Routes>
+		</BrowserRouter>
+
+      </>
+      
+
+    </div>
+  );
+}
+
+export default App;
